6f177f52061ea5ae5aa69a031955488fd7696601
chen <1228655653@qq.com>
2019-04-11 11:28:54 +0800

    代码上传

---
+++ demo1/index.js
@@ -0,0 +1,28 @@
+
+const hrefList = ['index1.html', 'index2.html', 'index3.html', 'index4.html', 'index5.html'];
+const Index = window.location.href.split("index")[1][0];
+
+function aa (state) {
+    if(state !== "true" || !state) {
+        document.getElementById("style" +Index).style.display="block";
+    } else if(Index < hrefList.length) {
+        window.location.href= hrefList[Index];
+    } else {
+        var spacetime = new Date().getTime() - sessionStorage.getItem('time');
+
+        //计算出相差天数
+        var days=Math.floor(spacetime/(24*3600*1000))
+        
+        //计算出小时数
+        var leave1=spacetime%(24*3600*1000)    //计算天数后剩余的毫秒数
+        var hours=Math.floor(leave1/(3600*1000))
+        //计算相差分钟数
+        var leave2=leave1%(3600*1000)        //计算小时数后剩余的毫秒数
+        var minutes=Math.floor(leave2/(60*1000))
+        //计算相差秒数
+        var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+        var seconds=Math.round(leave3/1000)
+        alert(" 用时 "+minutes+" 分钟"+seconds+" 秒")
+        window.location.href = "index.html";
+    }
+}
