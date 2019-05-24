45cd066463e9344808c8cd022717172f58c0a9d4
chen <1228655653@qq.com>
2019-05-24 18:34:54 +0800

    添加内容

--- 音频5s+字母变色进度条/index.js
+++ 音频5s+字母变色进度条/index.js
@@ -1,12 +1,12 @@
 
     const hrefList = ['index1.html', 'index2.html', 'index3.html', 'index4.html', 'index5.html'];
     const Index = window.location.href.split("index")[1][0];
-    clearInterval(timer);
-    var pg = document.getElementById('pg');
-    var timer = setInterval(function(e){
-        if(pg.value!=10) pg.value++;
-        else Index < hrefList.length ? pg = 10 : computeTime();
-    },1000);
+    // clearInterval(timer);
+    // var pg = document.getElementById('pg');
+    // var timer = setInterval(function(e){
+    //     if(pg.value!=10) pg.value++;
+    //     else Index < hrefList.length ? pg = 10 : computeTime();
+    // },1000);
 
     function aa (state) {
         if(!state) {
@@ -14,7 +14,7 @@
             document.getElementById("member").style.display="none";
             document.getElementById("style" +Index).style.display="block";
         } else if(state == "false") {
-            clearInterval(timer);
+            // clearInterval(timer);
             history.go(0);
         } else if(Index < hrefList.length) {
             document.getElementById("word").style.display="block";
