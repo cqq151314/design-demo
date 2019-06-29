0d6f8ffc939bbd19d79ef730f672edd729b05986
chen <1228655653@qq.com>
2019-06-29 14:22:17 +0800

    重第十个单词后不显示第二页的问题

--- 排序/index.js
+++ 排序/index.js
@@ -1,6 +1,6 @@
 
 const hrefList = ['index1.html', 'index2.html', 'index3.html', 'index4.html', 'index5.html', 'index6.html', 'index7.html', 'index8.html', 'index9.html', 'index10.html', 'index11.html', 'index12.html', 'index13.html', 'index14.html', 'index15.html', 'index16.html'];
-const Index = window.location.href.split("index")[1][0];
+const Index = window.location.href.split("index")[1].split('.')[0];
 
 function aa (state) {
     if(!state) {
